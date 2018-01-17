0a5d2299c430d137296f20178dee94ceaacb1f83
邢光辉 <xinggh@infore.com>
2018-01-17 13:12:35 +0800

    Fix: 字典表小类管理

---
+++ pages/system/dictionary/api.js
@@ -0,0 +1,89 @@
+// 字典表
+export function getDictionaries(params) {
+  return unfetch({
+    url: '/dictionary/list',
+    method: 'get',
+    params
+  })
+}
+
+export function updateDictionary(params) {
+  return unfetch({
+    url: '/dictionary',
+    method: 'put',
+    params
+  })
+}
+
+export function deleteDictionary(params) {
+  return unfetch({
+    url: '/dictionary',
+    params,
+    method: 'delete'
+  })
+}
+
+export function validDictionary(params) {
+  return unfetch({
+    url: '/dictionary/valid',
+    method: 'get',
+    params
+  })
+}
+
+export function saveDictionary(params) {
+  return unfetch({
+    url: '/dictionary',
+    method: 'post',
+    data: params
+  })
+}
+
+
+export function getDictionaryDatas(params) {
+  return unfetch({
+    url: '/dictionary/datas',
+    method: 'get',
+    params
+  })
+}
+
+export function validDictionaryData(params) {
+  return unfetch({
+    url: '/dictionary/data/valid',
+    method: 'get',
+    params
+  })
+}
+
+export function getDictionaryDataByCode(params) {
+  return unfetch({
+    url: '/dictionary/code',
+    method: 'get',
+    params
+  })
+}
+
+export function saveDictionaryData(params) {
+  return unfetch({
+    url: '/dictionary/data',
+    method: 'post',
+    data: params
+  })
+}
+
+export function updateDictionaryData(params) {
+  return unfetch({
+    url: '/dictionary/data',
+    method: 'put',
+    params
+  })
+}
+
+export function deleteDictionaryData(params) {
+  return unfetch({
+    url: '/dictionary/data',
+    params,
+    method: 'delete'
+  })
+}
