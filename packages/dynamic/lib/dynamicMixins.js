9f73cc2a06d269a9cdc20d3ffdab7d520f599032
wanghui <wanghui2@infore.com>
2018-01-29 15:01:58 +0800

    新增 动态台账 和 demo

---
+++ packages/dynamic/lib/dynamicMixins.js
@@ -0,0 +1,44 @@
+export default {
+    data() {
+        return {
+            currentTab: {},
+        }
+    },
+    methods: {
+        handleTabClick (tab) {
+            this.currentTab = tab
+            this.$router.push({ path: this.$route.fullPath, query: tab.query || {} })
+        }
+    },
+    computed: {
+        realComponent () {
+            const self = this
+            const { currentTab } = self
+            const { path } = currentTab
+            const { flatRoutes } = window.nenv
+            if (/https?:\/\//.exec(path)) {
+                return 'nv-iframe'
+            } else {
+                for (let flatRoute of flatRoutes) {
+                    if (flatRoute.path === path) {
+                        return flatRoute.component
+                    }
+                }
+            }
+        },
+        realComponentProps () {
+            const self = this
+            const { currentTab } = self
+            const { path } = currentTab
+            if (/https?:\/\//.exec(path)) {
+                return {
+                    nvUrl : path,
+                    ...currentTab.query
+                }
+            } else {
+
+            }
+            
+        }
+    }
+}
