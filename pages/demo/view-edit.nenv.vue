bb3dbcbee7e318830a875245b8baedb507b6caab
wanghui <wanghui2@infore.com>
2018-01-31 10:55:55 +0800

    新增可编辑视图功能

---
+++ pages/demo/view-edit.nenv.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <nv-checkbox nv-code="HOBBY" v-model="nvCheckBoxResult"/>
+        <nv-checkbox nv-code="HOBBY" v-model="nvCheckBoxResult" :disabled = "false"/>
+    </div>
+</template>
+<script>
+export default {
+    name: 'formEdit',
+    data () {
+        return {
+            nvCheckBoxResult: []
+        }
+    },
+    created () {
+        this.fetchForm()
+    },
+    methods: {
+        fetchForm () {
+            const self = this
+            unfetch('/view-edit', {
+                params: {
+                    CODE: 'x'
+                }
+            }).then(({data}) => {
+                self.nvCheckBoxResult = data.nvCheckBoxResult
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
