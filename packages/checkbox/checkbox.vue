2fed2ff2f7ed5dc1e2b76c47ddd9fd4b757ec684
wanghui <wanghui2@infore.com>
2018-01-31 10:58:07 +0800

    新增chekbox 的可编辑

--- packages/checkbox/checkbox.vue
+++ packages/checkbox/checkbox.vue
@@ -1,10 +1,11 @@
 <template>
-    <el-checkbox-group v-model="currentValue">
+    <el-checkbox-group v-model="currentValue" :disabled = "isNvDisabled" :class="{'is-nv-disabled': isNvDisabled}">
         <el-checkbox v-for="option in nvOptions" :label="option.VALUE" :key="option.VALUE">{{ option.NAME }}</el-checkbox>
     </el-checkbox-group>
 </template>
 <script>
 import nvInpterMixins from 'nenv/mixins/inputerMixins'
+// import nvEditViewMixins from 'nenv/mixins/editViewMixins'
 
 export default {
    name: 'NvCheckbox',
@@ -13,6 +14,10 @@ export default {
         value: {
             type: Array,
             required: true
+        },
+        disabled: {
+            type: Boolean,
+            default: undefined
         }
    },
    data () {
@@ -20,10 +25,26 @@ export default {
             nvOptions: []
         }
    },
+   computed: {
+       isNvDisabled () {
+           const self = this
+           return self.disabled !== undefined ? self.disabled : self.$route.query['nv-view'] === 'true'
+       }
+   },
 }
 </script>
+
 <style lang="scss" scoped>
 
 </style>
+
+<style lang="scss">
+    .el-checkbox__input {
+        &.is-nv-disabled  + .el-checkbox__label {
+            cursor: default;
+        }
+    }
+</style>
+
 
 
