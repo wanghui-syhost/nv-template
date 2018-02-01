3ab10b48a605995dd07d7f0436ace139cd56e473
wanghui <wanghui2@infore.com>
2018-02-01 17:17:49 +0800

    提交颜色

--- packages/dynamic/dynamic-ledger/components/ledger.js
+++ packages/dynamic/dynamic-ledger/components/ledger.js
@@ -51,6 +51,8 @@ export default {
                 self.isLoading = false
                 self.menus = data
                 self.handleTabClick(data[0])
+            }).catch(() => {
+                self.$emit('error')
             })
         },
         handleTabClick (tab) {
