8b56e57f52eb4a442d41f9a8b586bf134afdf713
wanghui <wanghui2@infore.com>
2018-02-05 11:03:35 +0800

    首页 home-block

---
+++ pages/home/components/todo.vue
@@ -0,0 +1,15 @@
+<template>
+    <home-block title="待办">
+    </home-block>
+</template>
+<script>
+import homeBlock from '../home-block'
+export default {
+    components: {
+        homeBlock
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/home/components/weather.vue
@@ -0,0 +1,15 @@
+<template>
+    <home-block title="天气">
+    </home-block>
+</template>
+<script>
+import homeBlock from '../home-block'
+export default {
+    components: {
+        homeBlock
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
