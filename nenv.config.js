0f1815977d1fc429e93d4dd87f07643b25adf3db
wanghui <wanghui2@infore.com>
2018-02-24 14:24:56 +0800

    修改静态资源无法找到的问题

--- nenv.config.js
+++ nenv.config.js
@@ -1,12 +1,14 @@
+const webpack = require('webpack')
 module.exports = {
+  assetPublicPath: '/platform/',
   project: {
     // 定义项目的标题
     title: "盈峰环境智慧水务平台",
   },
   proxy: {
     '/api/*': {
-      //target: 'http://192.168.37.6:8089', // 平台
-      target: 'http://192.168.31.22:8089', // 刑光辉
+      target: 'http://192.168.37.6:8089', // 平台
+      //target: 'http://192.168.31.22:8089', // 刑光辉
       // target: 'http://localhost:8080',// 本地
       filter(pathname, req) {
         if (pathname.match(/\/api/)) {
@@ -15,8 +17,8 @@ module.exports = {
       }
     }
   },
-  /* webpack(config, { dev }) {
-    if (dev) { // 开发环境配置
+  webpack(config, { dev }) {
+    if (true) { // 开发环境配置
       config.plugins.push(
         new webpack.DefinePlugin({
           'process.env.BASE_URL': "'http://192.168.37.6:8089/api'"
@@ -26,5 +28,5 @@ module.exports = {
       
      }
     return config
-  } */
+  }
 }
