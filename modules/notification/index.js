8a02c1acda893099840af43919b08e07a5790cc8
wanghui <wanghui2@infore.com>
2018-03-13 15:01:16 +0800

    去掉 消息通知

--- modules/notification/index.js
+++ modules/notification/index.js
@@ -1,11 +1,11 @@
 import { Notification } from 'element-ui'
 
-new Notification({
-    type: 'success',
-    title: 'hah',
-    message: '这就是内容了啊',
-    onClick: function () {
-        console.log(this)
-    }
-    // showClose: false
-})
+// new Notification({
+//     type: 'success',
+//     title: 'hah',
+//     message: '这就是内容了啊',
+//     onClick: function () {
+//         console.log(this)
+//     }
+//     // showClose: false
+// })
