dd71a04aa13b970a5164adda925ec9bf8f334632
wanghui <wanghui2@infore.com>
2018-03-21 15:16:31 +0800

    下拉选择新增 all 选项

--- packages/select/select.vue
+++ packages/select/select.vue
@@ -1,7 +1,7 @@
 <template>
     <el-select v-model="currentValue" placeholder="请选择" :disabled = "isNvDisabled" :class="{'is-nv-disabled': isNvDisabled}">
         <el-option
-        v-for="option in nvOptions"
+        v-for="option in nvOptionsExt"
         :key="option.VALUE"
         :label="option.NAME"
         :value="option.VALUE">
@@ -15,20 +15,41 @@ export default {
     mixins: [nvInpterMixins],
     props: {
         value: {
-            type: String,
+            // type: String,
             required: true
         },
         disabled: {
             type: Boolean,
             default: undefined
+        },
+        canAllSelect: {
+            type: Boolean,
+            default: true,
+        },
+        allSelectLabel: {
+            type: String,
+            default: '全部'
         }
     },
     data () {
         return {
-            nvOptions: []
+            nvOptions: [
+
+            ]
         }
     },
     computed: {
+        nvOptionsExt () {
+            const self = this
+            const { canAllSelect } = self
+            return (canAllSelect ? [
+                        {
+                            ID: '-1',
+                            NAME: self.allSelectLabel,
+                            VALUE: null,
+                        }
+                    ] : []).concat(self.nvOptions)
+        },
        isNvDisabled () {
            const self = this
            return self.disabled !== undefined ? self.disabled : self.$route.query['nv-view'] === 'true'
@@ -43,5 +64,3 @@ export default {
         }
     }
 </style>
-
-
