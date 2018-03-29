a77f6f2d14aaa4509251b03be957f016ea8a175b
wanghui <wanghui2@infore.com>
2018-03-29 11:29:08 +0800

    新增i18n

---
+++ pages/demo/interpolation.nenv.vue
@@ -0,0 +1,15 @@
+<template>
+    <nv-layout>
+        {{ $t('common.USER_NAME') }}
+    </nv-layout>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
+
+
