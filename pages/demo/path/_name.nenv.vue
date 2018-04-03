4b0a3149dd58b4596fdc86909e4fa794e1532bb6
wanghui <wanghui2@infore.com>
2018-04-03 15:35:45 +0800

    新增路径变量

---
+++ pages/demo/path/_name.nenv.vue
@@ -0,0 +1,17 @@
+<template>
+    <nv-layout>
+        {{ $route.params.name }}
+    </nv-layout>
+</template>
+<script>
+export default {
+    created () {
+        alert(`路径变量为： ${this.$route.params.name} `)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
