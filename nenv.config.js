1c977d8143089fd88c246eb9d3386a71a38e232f
xiachengwei5 <xiachengwei5@163.com>
2018-04-08 20:55:49 +0800

    修改配置文件

--- nenv.config.js
+++ nenv.config.js
@@ -11,7 +11,7 @@ module.exports = {
     '/api/*': { //平台api管理
       // target: 'http://192.168.37.6:8089', // 平台
       // target: 'http://192.168.31.22:8089', // 邢光辉 ,   
-      target: 'http://localhost:8080',// 本地
+      target: 'http://localhost:8089',// 本地
       filter(pathname, req) {
         if (pathname.match(/\/api/)) {
           return true
@@ -19,7 +19,7 @@ module.exports = {
       }
     },  
     '/file/*': { // 平台文件管理
-      target: 'http://192.168.37.6:8088', 
+      target: 'http://192.168.37.6:8808', 
       filter(pathname, req) {
         if (pathname.match(/\/file/)) {
           return true
