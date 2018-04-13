c9f0a7c5aba7ffffda5336861c9dfdf15bb99cdc
wanghui <wanghui2@infore.com>
2018-04-13 13:28:10 +0800

    新增动态选项卡页面

---
+++ pages/dynamic-tab/_code.nenv.vue
@@ -0,0 +1,20 @@
+<template>
+    <nv-layout>
+        <nv-dynamic-tab :nvCode="nvCode"/>
+    </nv-layout>
+</template>
+<script>
+export default {
+    name: 'DynamicTab',
+    computed: {
+        nvCode () {
+            return this.$route.query['nv-code'] || this.$route.params.code
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+
+
