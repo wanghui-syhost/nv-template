61c261a9f54d912dbbebb9714aacc431bb2a5a66
wanghui <wanghui2@infore.com>
2018-05-11 16:38:06 +0800

    允许动态台账内使用 动态选项卡

--- packages/dynamic/lib/dynamicMixins.js
+++ packages/dynamic/lib/dynamicMixins.js
@@ -2,6 +2,7 @@ export default {
     data() {
         return {
             currentTab: {},
+            realComponentProps: {}
         }
     },
     methods: {
@@ -16,29 +17,38 @@ export default {
             const { currentTab } = self
             const { path } = currentTab
             const { flatRoutes } = window.nenv
-        	if (/https?:\/\//.exec(path)) {
-                return 'nv-iframe'
-            } else {
-                for (let flatRoute of flatRoutes) {
-                    if (flatRoute.path === path) {
-                        return flatRoute.component
+            if (path) {
+                if (/https?:\/\//.exec(path)) {
+                    this.realComponentProps = { nvUrl: path }
+                    return 'nv-iframe'
+                } else {
+                    const match = this.$router.match(path)
+                    console.log(match)
+                    const { matched, params } = match
+                    const { components } = matched[matched.length - 1]
+
+                    this.realComponentProps = {
+                        'is-Daynamic': true,
+                        nvProps: params
                     }
+
+                    return components.default || components
                 }
             }
             
         },
-        realComponentProps () {
-            const self = this
-            const { currentTab } = self
-            const { path } = currentTab
-        	if (/https?:\/\//.exec(path)) {
-                return {
-                    nvUrl : path,
-                    ...currentTab.query
-                }
-            } else {
+        // realComponentProps () {
+        //     const self = this
+        //     const { currentTab } = self
+        //     const { path } = currentTab
+        // 	if (/https?:\/\//.exec(path)) {
+        //         return {
+        //             nvUrl : path,
+        //             ...currentTab.query
+        //         }
+        //     } else {
 
-            }
-        }
+        //     }
+        // }
     }
 }
